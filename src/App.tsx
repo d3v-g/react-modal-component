fcf4c15f38253e51e1d2a9151d42ddcf6ccf65c0
d3v-g <wang.jiaye.ti@gmail.com>
2023-09-28 21:49:00 +0100

    feat: enabled switching from one modal to next

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { useState } from "react"
 import "./App.scss"
 import Button from "./components/Button"
 import Modal from "./components/Modal"
-import ModalController from "./components/ModalController"
 
 function App() {
     const firstModalTitle =
@@ -28,47 +28,37 @@ function App() {
         nisi nulla finibus nibh, nec fringilla nunc urna\
         fermentum diam."
 
+    const [openedModalId, setOpenedModalId] = useState<number>()
+
     return (
         <div className="app">
-            <ModalController
-                controlElement={(handleClick) => (
-                    <Button
-                        handleClick={handleClick}
-                        label="Open first modal"
-                    />
-                )}
-                modalElement={(handleClose) => (
-                    <Modal
-                        title={firstModalTitle}
-                        description={firstModalDescription}
-                    >
-                        <div className="modal__buttons">
-                            <button onClick={handleClose}>Cancel</button>
-                            <button onClick={handleClose}>Continue</button>
-                        </div>
-                    </Modal>
-                )}
+            <Button
+                handleClick={() => setOpenedModalId(1)}
+                label="Open first modal"
             />
 
-            <ModalController
-                controlElement={(handleClick) => (
-                    <Button
-                        handleClick={handleClick}
-                        label="Open second modal"
-                    />
-                )}
-                modalElement={(handleClose) => (
-                    <Modal
-                        title={secondModalTitle}
-                        description={secondModalDescription}
-                    >
-                        <div className="modal__buttons">
-                            <button onClick={handleClose}>Cancel</button>
-                            <button onClick={handleClose}>Continue</button>
-                        </div>
-                    </Modal>
-                )}
+            <Modal
+                title={firstModalTitle}
+                description={firstModalDescription}
+                isOpen={openedModalId === 1}
+            >
+                <button onClick={() => setOpenedModalId(0)}>Cancel</button>
+                <button onClick={() => setOpenedModalId(2)}>Continue</button>
+            </Modal>
+
+            <Button
+                handleClick={() => setOpenedModalId(2)}
+                label="Open second modal"
             />
+
+            <Modal
+                title={secondModalTitle}
+                description={secondModalDescription}
+                isOpen={openedModalId === 2}
+            >
+                <button onClick={() => setOpenedModalId(0)}>Cancel</button>
+                <button onClick={() => setOpenedModalId(1)}>Continue</button>
+            </Modal>
         </div>
     )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,19 +1,30 @@
 import { ReactNode } from "react"
 
 export default function Modal({
+    isOpen,
     title,
     description,
     children,
 }: {
+    isOpen: boolean
     title: string
     description: string
     children: ReactNode
 }) {
+    if (!isOpen) return null
+
     return (
-        <div className="modal__content">
-            <h1>{title}</h1>
-            <p>{description}</p>
-            {children}
+        // full-screen wrapper
+        <div aria-describedby="modal description" className="modal__container">
+            <div className="modal__content" role="dialog">
+                <div
+                    className="modal__description--screenreader"
+                    id="modal description"
+                ></div>
+                <h1>{title}</h1>
+                <p>{description}</p>
+                <div>{children}</div>
+            </div>
         </div>
     )
 }
